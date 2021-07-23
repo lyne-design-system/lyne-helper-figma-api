fd99aa8e421990292488e74205fae85a346b020a
Yves Tscherry <info@feerglas.com>
2021-07-23 10:11:27 +0200

    docs: remove comments

--- src/figmaFrames.js
+++ src/figmaFrames.js
@@ -1,9 +1,6 @@
 const shouldIgnoreFrame = (frame, frameIgnorePattern) => frame.name.indexOf(frameIgnorePattern) === 0;
 
-// Get frames from 1st page of the Figma file
 module.exports = (figmaPage, frameIgnorePattern) => {
-
-  // make sure that 1st page has regions
   const {
     children
   } = figmaPage;
